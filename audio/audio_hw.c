351bad64a4d3d8b545b36535659618035ebcbab6
Andreas Schneider <asn@cryptomilk.org>
2015-10-14 09:34:29 +0200

    audio: move the audio route reset down

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -304,11 +304,11 @@ static void select_devices(struct audio_device *adev)
     char input_gain[64] = {0};
     int new_route_id;
 
-    audio_route_reset(adev->ar);
-
     new_route_id = (1 << (input_source_id + OUT_DEVICE_CNT)) + (1 << output_device_id);
-    if (new_route_id == adev->cur_route_id)
+    if (new_route_id == adev->cur_route_id) {
+        ALOGV("%s: Routing hasn't changed, leaving function.", __func__);
         return;
+    }
     adev->cur_route_id = new_route_id;
 
     if (input_source_id != IN_SOURCE_NONE) {
@@ -342,6 +342,8 @@ static void select_devices(struct audio_device *adev)
           adev->out_device, adev->input_source,
           output_route ? output_route : "none",
           input_route ? input_route : "none");
+
+	audio_route_reset(adev->ar);
 
     if (output_route) {
         audio_route_apply_path(adev->ar, output_route);
@@ -549,6 +551,7 @@ static void stop_call(struct audio_device *adev)
 
     /* Do not change devices if we are switching to WB */
     if (adev->mode != AUDIO_MODE_IN_CALL) {
+        ALOGV("%s: Reset route to default", __func__);
         adev->input_source = AUDIO_SOURCE_DEFAULT;
 
         select_devices(adev);
