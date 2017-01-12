186ee605d9beaafc700f5e7fa51115d625f7efc8
deadman96385 <seanhoyt963@gmail.com>
2017-01-12 13:54:52 -0600

    doze: Use ambient display

--- hero-common.mk
+++ hero-common.mk
@@ -104,7 +104,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	libsamsung_symbols \
 	SamsungServiceMode \
-	Torch
+	Torch \
+	SamsungDoze
 
 ###########################################################
 ### DTB TOOL
