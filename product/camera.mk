c33e25dc11bf1ea06f2d50c10365f01afc4d2e15
deedwar <deed821@gmail.com>
2017-04-23 13:21:48 +1000

    hero: force the use of HAL3

--- product/camera.mk
+++ product/camera.mk
@@ -10,3 +10,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
 	frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml
+
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.camera.HAL3.enabled=1
