8a255c7da92122ceffcc7ad7639efc275026db9e
deedwar <deed821@gmail.com>
2017-05-18 14:28:53 +1000

    audio: fix various routing issues

--- product/audio.mk
+++ product/audio.mk
@@ -3,6 +3,7 @@ DEVICE_PATH := device/samsung/hero-common
 # Configs
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
+	$(DEVICE_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
 	$(DEVICE_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
 
 # Packages
