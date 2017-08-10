c3b77f346d512dc9767ad09e9d6e9124f66052fe
Andrw0830 <Andrw0830@gmail.com>
2017-08-10 19:33:12 -0400

    audio: use old HAL

--- product/audio.mk
+++ product/audio.mk
@@ -4,14 +4,16 @@ DEVICE_PATH := device/samsung/hero-common
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
 	$(DEVICE_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
-	$(DEVICE_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
+	$(DEVICE_PATH)/configs/audio/mixer_gains.xml:system/etc/mixer_gains.xml \
+	$(DEVICE_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Packages
 PRODUCT_PACKAGES += \
-	audio.a2dp.default \
-	audio.usb.default \
-	audio.r_submix.default \
-	audio.primary.universal8890
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    audio.primary.universal8890 \
+    libtinycompress
 
 # Permissions
 PRODUCT_COPY_FILES += \
