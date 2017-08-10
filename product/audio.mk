fb26b09dad1af7d403984a7bdeab394aaa2d0fa0
Andrw0830 <Andrw0830@gmail.com>
2017-08-10 19:33:12 -0400

    hero: use modified old audio HAL
    Change-Id: I4fd29f1d6f5ef0ef564109d7ad9261c04c3b627e

--- product/audio.mk
+++ product/audio.mk
@@ -3,14 +3,15 @@ DEVICE_PATH := device/samsung/hero-common
 # Configs
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-	$(DEVICE_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
+	$(DEVICE_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Packages
 PRODUCT_PACKAGES += \
-	audio.a2dp.default \
-	audio.usb.default \
-	audio.r_submix.default \
-	audio.primary.universal8890
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    audio.primary.universal8890 \
+    libtinycompress
 
 # Permissions
 PRODUCT_COPY_FILES += \
