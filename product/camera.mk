50c02595a2815334afa04990b0bcbbb415b44e26
warren <deed821@gmail.com>
2017-08-19 05:26:22 +1000

    camera: update wrapper

--- product/camera.mk
+++ product/camera.mk
@@ -9,7 +9,3 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
 	frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml
-
-# build.prop
-PRODUCT_PROPERTY_OVERRIDES += \
-	camera.hal1.packagelist=com.skype.raider,com.whatsapp,com.cyworld.camera
