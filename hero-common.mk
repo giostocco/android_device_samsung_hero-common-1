a48e8b023d39c4d55fed6260fdfa32c2daf394f5
warren <deed821@gmail.com>
2017-08-19 14:15:51 +1000

    hero: tidy up the mk file

--- hero-common.mk
+++ hero-common.mk
@@ -1,37 +1,16 @@
-#
-# Copyright (C) 2017 Fernando Von Arx <fer.vonarx@gmail.com>
-# Copyright (C) 2017 Jesse Chan <cjx123@outlook.com>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-
 # Common Overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/hero-common/overlay
 
 LOCAL_PATH := device/samsung/hero-common
 
-###########################################################
-### RAMDISK
-###########################################################
-
+# Ramdisk
 PRODUCT_PACKAGES += \
 	fstab.samsungexynos8890 \
 	init.samsung.rc \
 	init.samsungexynos8890.rc \
 	ueventd.samsungexynos8890.rc \
 
-###########################################################
-### PERMISSONS
-###########################################################
-
+# Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
@@ -42,10 +21,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
 	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
-###########################################################
-### GRAPHICS
-###########################################################
-
+# Graphics
 # This device is xhdpi.  However the platform doesn't
 # currently contain all of the bitmaps at xhdpi density so
 # we do this little trick to fall back to the hdpi version
@@ -59,58 +35,38 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-###########################################################
-### POWER
-###########################################################
-
+# Power
 PRODUCT_PACKAGES += \
 	power.universal8890
 
-###########################################################
-### LIGHTS
-###########################################################
-
+# Lights
 PRODUCT_PACKAGES += \
 	lights.universal8890
 
-###########################################################
-### CHARGER
-###########################################################
-
+# Charger
 # Offmode charger
 # Use cm images if available, aosp ones otherwise
 PRODUCT_PACKAGES += \
 	charger_res_images \
 	cm_charger_res_images
 
-###########################################################
-### MOBICORE
-###########################################################
-
+# Mobicore
 PRODUCT_PACKAGES += \
 	keystore.exynos5
 
 
-###########################################################
-### WIDEVINE
-###########################################################
+# widevine
 PRODUCT_PACKAGES += \
     libshim_wvm
 
-###########################################################
-### PACKAGES
-###########################################################
-
+#Packages
 PRODUCT_PACKAGES += \
 	libsamsung_symbols \
 	SamsungServiceMode \
 	Torch \
 	SamsungDoze
 
-###########################################################
-### DTB TOOL
-###########################################################
-
+# DTB Tool
 PRODUCT_PACKAGES += \
 	dtbhtoolExynos
 
