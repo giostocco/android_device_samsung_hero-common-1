9d8884cd1170b83c32bd0f517ce170c8ea9afed1
Sascha <nesterovic5@gmail.com>
2017-09-03 09:24:00 +0000

    Merge :"https://github.com/evolve-hero2lte/android_device_samsung_hero-common"
    Change-Id: I64dd462c09b9ee196f51a826219bff3c83364698

--- product/graphics.mk
+++ product/graphics.mk
@@ -17,6 +17,25 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.opengles.version=196609 \
 	ro.sf.lcd_density=540
 
+# GPU
+PRODUCT_PROPERTY_OVERRIDES += \
+	debug.composition.type=dyn \
+	debug.hwc.max_hw_overlays=8 \
+	ro.opengles.version=196610 \
+	debug.sf.hw=0 \
+	persist.sys.ui.hw=0 \
+	video.accelerate.hw=1 \
+	debug.egl.profiler=1 \
+	debug.egl.hw=1
+
+# Surface
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.scrollingcache=4 \
+	persist.sys.purgeable_assets=1 \
+	ro.compcache.default=18 \
+	ro.max.fling_velocity=12000 \
+	ro.min.fling_velocity=8000
+
 # Properties - HWC - not used on cm/aosp
 PRODUCT_PROPERTY_OVERRIDES += \
 	debug.hwc.winupdate=1 \
